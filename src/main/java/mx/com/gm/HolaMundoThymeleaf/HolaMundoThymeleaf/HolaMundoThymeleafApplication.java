3955cab750abb461b6f263f4986fc82fbe70f109
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-18 18:54:29 -0500

    first commit

---
+++ src/main/java/mx/com/gm/HolaMundoThymeleaf/HolaMundoThymeleaf/HolaMundoThymeleafApplication.java
@@ -0,0 +1,13 @@
+package mx.com.gm.HolaMundoThymeleaf.HolaMundoThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/gm/HolaMundoThymeleaf/HolaMundoThymeleaf/HolaMundoThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.gm.HolaMundoThymeleaf.HolaMundoThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
