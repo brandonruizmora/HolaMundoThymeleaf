d6e8f1cd49e9b911ffb5655ff4c27ee025139c3a
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-18 20:47:09 -0500

    Creacion de una lista de personas

--- src/main/java/mx/com/gm/HolaMundoThymeleaf/HolaMundoThymeleaf/ControladorInicio.java
+++ src/main/java/mx/com/gm/HolaMundoThymeleaf/HolaMundoThymeleaf/ControladorInicio.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//import java.util.ArrayList;
+import java.util.Arrays;
+//import java.util.List;
+
 @Controller
 @Slf4j
 public class ControladorInicio {
@@ -28,10 +32,27 @@ public class ControladorInicio {
         persona.setEmail("brandon.ruiz@hotmail.com");
         persona.setTelefono("456 345 4567");
 
+        var persona2 = new Persona();
+        persona2.setNombre("Fernanda");
+        persona2.setApellido("Madrigal");
+        persona2.setEmail("fernanda.madrigal@hotmail.com");
+        persona2.setTelefono("013 224 4567");
+
+        //List<Persona> personas = new ArrayList();
+        //personas.add(persona);
+        //personas.add(persona2);
+
+        //var personas = new ArrayList();
+        //personas.add(persona2);
+        //personas.add(persona);
+
+        var personas= Arrays.asList(persona, persona2);
+
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
         model.addAttribute("persona", persona);
-        
+        model.addAttribute("personas", personas);
+
         return "index";
     }
 }
