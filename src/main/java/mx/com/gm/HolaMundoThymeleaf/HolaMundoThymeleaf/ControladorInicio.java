9fe098904159e6e9a1722733f69d57c1d85d730d
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-18 20:01:40 -0500

    Compartir informacion con Spring MVC y Thymeleaf

--- src/main/java/mx/com/gm/HolaMundoThymeleaf/HolaMundoThymeleaf/ControladorInicio.java
+++ src/main/java/mx/com/gm/HolaMundoThymeleaf/HolaMundoThymeleaf/ControladorInicio.java
@@ -1,6 +1,8 @@
 package mx.com.gm.HolaMundoThymeleaf.HolaMundoThymeleaf;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.var;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class ControladorInicio {
 
+    @Value("${index.saludo}")
+    private String saludo;
+
     @GetMapping("/")
     public String inicio(Model model) {
         log.info("Ejecutando controlador Spring MVC");
-
+        var mensaje = "Hola Mundo con Thymeleaf";
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
         return "index";
     }
 }
