c0081564d25359ad53f0d64d7d7e3b2bb83fad14
brandonruizmora <brandon_ruizmor@outlook.es>
2022-07-18 20:26:24 -0500

    Creacion de clase de dominio Persona

--- src/main/java/mx/com/gm/HolaMundoThymeleaf/HolaMundoThymeleaf/ControladorInicio.java
+++ src/main/java/mx/com/gm/HolaMundoThymeleaf/HolaMundoThymeleaf/ControladorInicio.java
@@ -2,6 +2,7 @@ package mx.com.gm.HolaMundoThymeleaf.HolaMundoThymeleaf;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
+import mx.com.gm.HolaMundoThymeleaf.domain.Persona;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +17,21 @@ public class ControladorInicio {
 
     @GetMapping("/")
     public String inicio(Model model) {
+
         log.info("Ejecutando controlador Spring MVC");
+
         var mensaje = "Hola Mundo con Thymeleaf";
+
+        var persona = new Persona();
+        persona.setNombre("Brandon");
+        persona.setApellido("Ruiz");
+        persona.setEmail("brandon.ruiz@hotmail.com");
+        persona.setTelefono("456 345 4567");
+
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
+        model.addAttribute("persona", persona);
+        
         return "index";
     }
 }
